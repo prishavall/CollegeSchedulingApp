fc1449ad6c8a5b23df84e390b5e0cad457535c1c
kaavyamenon04 <55721332+kaavyamenon04@users.noreply.github.com>
2024-01-29 10:55:12 -0500

    Fixed nullpointer exceptions

--- app/src/main/java/com/example/emptyactivityapp/MainActivity.java
+++ app/src/main/java/com/example/emptyactivityapp/MainActivity.java
@@ -11,6 +11,7 @@ import com.example.emptyactivityapp.Model.ToDoModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView tasksRecyclerView;
@@ -23,7 +24,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         taskList = new ArrayList<>();
         tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
         tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
--- app/src/main/java/com/example/emptyactivityapp/SplashActivity.java
+++ app/src/main/java/com/example/emptyactivityapp/SplashActivity.java
@@ -6,13 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import java.util.Objects;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        getSupportActionBar().hide();
+        // Check if action bar is not null before hiding
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 
         final Intent i = new Intent(SplashActivity.this,MainActivity.class);
         new Handler().postDelayed(new Runnable() {
